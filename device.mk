a3fb8aef1400b72d34850544bf3fb48eaee1997b
Derek Sevilla <deryk0917@gmail.com>
2012-05-27 20:57:13 -0700

    Added back mpdecision thermald and thermald.conf Just for a double check

--- device.mk
+++ device.mk
@@ -112,6 +112,9 @@ PRODUCT_COPY_FILES += \
 # HTC BT Audio tune
 PRODUCT_COPY_FILES += device/htc/pyramid/configs/AudioBTID.csv:system/etc/AudioBTID.csv
 
+# QC thermald config
+PRODUCT_COPY_FILES += device/htc/pyramid/configs/thermald.conf:system/etc/thermald.conf
+
 # misc
 PRODUCT_COPY_FILES += \
     device/htc/pyramid/vold.fstab:system/etc/vold.fstab
