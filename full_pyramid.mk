2f8049352cab2576a2d22144f929505a4affc4cc
Derek Sevilla <deryk0917@gmail.com>
2012-06-08 13:34:41 -0700

    Removed compiling Torch as not needed

--- full_pyramid.mk
+++ full_pyramid.mk
@@ -24,10 +24,6 @@
 # GanOptimizer
 PRODUCT_PACKAGES += \
     GanOptimizer
-
-# Torch
-PRODUCT_PACKAGES += \
-    Torch
  
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
