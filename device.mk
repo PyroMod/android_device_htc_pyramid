7a8d0c8748e459b7ff0ea9af7af01cd247cfe2f4
Derek Sevilla <deryk0917@gmail.com>
2012-08-06 22:36:23 -0700

    Remove duplicate lines

--- device.mk
+++ device.mk
@@ -135,9 +135,6 @@ PRODUCT_COPY_FILES += \
     device/htc/pyramid/prebuilt/bcmdhd.ko:/system/lib/modules/bcmdhd.ko \
     device/htc/pyramid/prebuilt/scsi_wait_scan.ko:/system/lib/modules/scsi_wait_scan.ko
 
-# call the proprietary setup
-$(call inherit-product-if-exists, vendor/htc/pyramid/pyramid-vendor.mk)
-
 # media profiles and capabilities spec
 $(call inherit-product, device/htc/pyramid/media_a1026.mk)
 
